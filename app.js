eb30d3ed69552bd88aa30883ed692f7b911cceaf
snehasish <snehasish.bgc@gmail.com>
2024-09-27 21:06:17 +0600

    404 setup

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express();
 const taskRouter = require('./routes/tasks')
 const connectDb = require('./db/connection')
+const notFound = require('./not-found/not-found')
 require('dotenv').config()
 
 app.use(express.json())
 
 app.use('/api/v1/tasks', taskRouter)
+app.use(notFound)
 
 const port = 3000;
 
