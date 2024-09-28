1733f5a67e46064b9e6521dfc3935e34809ca2b4
snehasish <snehasish.bgc@gmail.com>
2024-09-28 11:48:46 +0600

    get port from env

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/api/v1/tasks', taskRouter)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
